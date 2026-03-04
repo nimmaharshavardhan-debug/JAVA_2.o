746e1c17abc1d21da30e2ca8d37f59bd75940302
Nimma Harsha Vardhan <nimmaharshavardhann@gmail.com>
2026-03-04 21:07:01 +0530

    This is my first data type program

---
+++ Data_Types/DecPrintPrimitDataTypes.java
@@ -0,0 +1,26 @@
+package Data_Types;
+public class DecPrintPrimitDataTypes {
+    public static void main(String [] args){
+        // Primitive Data Types 
+        byte  by =  123;
+        short sh = 10000;
+        int in = 20000000;
+        long l = 123000000000L;
+        float  fl = 12.675674F;
+        double dou = 12345.566666666;
+        char ch = 'A';
+        String str = "Hello";   // Non- Primitive data type
+        boolean bool = true;  // true  or false values 
+
+        System.out.println("Byte "+by);
+        System.out.println("Short "+sh);
+        System.out.println("Int "+in);
+        System.out.println("Long "+l);
+        System.out.println("Float "+fl);
+        System.out.println("Double "+dou);
+        System.out.println("Char "+ch);
+        System.out.println("String "+str);
+        System.out.println("Boolean "+bool);
+
+    }
+}
